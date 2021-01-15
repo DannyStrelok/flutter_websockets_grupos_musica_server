f901fbc80812033c013f190ade16515265a1b6d6
DannyStrelok <hmartindaniel@gmail.com>
2021-01-15 19:25:23 +0100

    Servidor websocket (socket.io) terminado

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const path = require('path');
+const Bands = require('./models/bands');
+const Band = require('./models/band');
 require('dotenv').config();
-
 
 const app = express();
 
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
+const bands = new Bands();
+
+bands.addBand(new Band('Slipknot'));
+bands.addBand(new Band('Limp bizkit'));
+bands.addBand(new Band('Adele'));
+bands.addBand(new Band('Aerosmith'));
+
 io.on('connection', client => {
     console.log('cliente conectado');
+
+    client.emit('grupos-activos', bands.getBands() );
+
     client.on('disconnect', () => {
         console.log('cliente desconectado');
     });
@@ -24,11 +35,23 @@ io.on('connection', client => {
 
     client.on('emitir-mensaje', (payload) => {
         console.log(payload);
-        io.emit('nuevo-mensaje', {
-            nombre: 'dani',
-            mensaje: 'que pasa tronco!!'
-        })
+        client.broadcast.emit('nuevo-mensaje', payload);
     });
+
+    client.on('vote-band', (data) => {
+        bands.voteBand(data.id);
+        io.emit('grupos-activos', bands.getBands() );
+    })
+
+    client.on('add-band', (data) => {
+        bands.addBand(new Band(data.name));
+        io.emit('grupos-activos', bands.getBands() );
+    })
+
+    client.on('delete-band', (data) => {
+        bands.deleteBand(data.id);
+        io.emit('grupos-activos', bands.getBands() );
+    })
 
 });
 
